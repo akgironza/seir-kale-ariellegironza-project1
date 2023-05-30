d09c2b0b9c013e3ad1188b5f74038ad085264df2
Arielle Gironza <ariellegironza@Arielles-Air.fios-router.home>
2023-05-30 19:33:40 -0400

    outlined two main API calls, confirmed returned data is what I want and successful return

--- js/app.js
+++ js/app.js
@@ -1,22 +1,28 @@
-// working 
+// API call: Get Recent Votes
 $.ajax({
-    url: "https://api.propublica.org/congress/v1/102-117/house/members.json",
-    beforeSend: function(xhr) { 
-      xhr.setRequestHeader("X-API-Key","Zcqysq7u2VyNWvhGaNDnW96LIyjjfSxMFGiiRP9j"); 
-    },
-
-    // things I thought I might need, didn't, but keeping just in case for now
-    // type: "GET",
-    // dataType: "json",
-    // contentType: "application/json",
-
-    // success function that was nice but is annoying every time I save
-    // success: function (data) {
-      // alert("success")
-      // console.log(data);
-    // },
-    error: function(){
-      alert("Unable to get data");
-    }
+  url: "https://api.propublica.org/congress/v1/house/votes/recent.json",
+  beforeSend: function(xhr) { 
+    xhr.setRequestHeader("X-API-Key","Zcqysq7u2VyNWvhGaNDnW96LIyjjfSxMFGiiRP9j");
+},
+  success: function (data){
+    console.log(data);
+  },
+  error: function(){
+    alert: ("Unable to get data");
+  }
 });
 
+// API call: Get a Specific Roll Call Vote
+// testing with a specific roll-call number currently
+$.ajax({
+  url: "https://api.propublica.org/congress/v1/118/house/sessions/1/votes/237.json",
+  beforeSend: function(xhr) { 
+    xhr.setRequestHeader("X-API-Key","Zcqysq7u2VyNWvhGaNDnW96LIyjjfSxMFGiiRP9j");
+},
+  success: function (data){
+  console.log(data);
+},
+  error: function(){
+  alert: ("Unable to get data");
+}
+});
