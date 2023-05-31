2430516b1eb162175829a2a61773fd5bf11ebce0
Arielle Gironza <ariellegironza@Arielles-Air.fios-router.home>
2023-05-30 21:47:45 -0400

    updated API calls, some start to HTML

--- js/app.js
+++ js/app.js
@@ -3,26 +3,25 @@ $.ajax({
   url: "https://api.propublica.org/congress/v1/house/votes/recent.json",
   beforeSend: function(xhr) { 
     xhr.setRequestHeader("X-API-Key","Zcqysq7u2VyNWvhGaNDnW96LIyjjfSxMFGiiRP9j");
-},
-  success: function (data){
-    console.log(data);
   },
   error: function(){
-    alert: ("Unable to get data");
-  }
-});
+  alert: ("Unable to get data");
+}
+}).then((data) => {
+  console.log(data);
+})
 
 // API call: Get a Specific Roll Call Vote
 // testing with a specific roll-call number currently
+
 $.ajax({
   url: "https://api.propublica.org/congress/v1/118/house/sessions/1/votes/237.json",
   beforeSend: function(xhr) { 
     xhr.setRequestHeader("X-API-Key","Zcqysq7u2VyNWvhGaNDnW96LIyjjfSxMFGiiRP9j");
-},
-  success: function (data){
-  console.log(data);
-},
+  },
   error: function(){
   alert: ("Unable to get data");
 }
-});
+}).then((data) => {
+  console.log(data);
+})
