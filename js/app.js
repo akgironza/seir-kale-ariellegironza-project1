e9d1fffc33b1fd4b39a245566aea67b3d62d5a01
Arielle Gironza <ariellegironza@Arielles-Air.fios-router.home>
2023-06-03 03:21:32 -0400

    vote data populating on page & vote details being stored for later search

--- js/app.js
+++ js/app.js
@@ -22,12 +22,14 @@ $.ajax({
   const recentRollCalls = recentVotesData.map(i => i.roll_call)
   console.log(recentRollCalls);
 
-
   // API call: Get a Specific Roll Call Vote
-  // I think I want to do a callback/loop for this API call
+  // Callback for this API call to go over the previous data received
     // need to swap out the roll_call number in the url
-  
+    // need to create a new section in the webpage for each vote
+
   function pullRollCalls(rollCall){
+    const $votesContainer = $(".votesContainer");
+    const voteDetails = [];
     $.ajax({
       url: `https://api.propublica.org/congress/v1/118/house/sessions/1/votes/${rollCall}.json`, 
       beforeSend: function(xhr) { 
@@ -38,24 +40,32 @@ $.ajax({
     error: function(){
       alert: ("Unable to get data");
     }
-  }).then((data) => {
-    // console.log(rollCall)
-    console.log(data);
+  }).then((response) => {
+    console.log(rollCall);
+    //console.log(response);
+    voteDetails.push(response);
+    // empty the container?
+    //$votesContainer.empty()
+    $votesContainer.append($("<div>").text(
+      `${response.results.votes.vote.date} ||
+      ${response.results.votes.vote.bill.number}||
+      ${response.results.votes.vote.bill.title} ||
+      ${response.results.votes.vote.question} ||
+      Additional details: ${response.results.votes.vote.description} ||
+      ${response.results.votes.vote.result} ||`
+      ));
+    console.log(voteDetails);
   })
   }
-  recentRollCalls.forEach(pullRollCalls)
-  // the fact that this is working now... kinda don't want to touch this anymore
-  // but I will continue
-})
+recentRollCalls.forEach(pullRollCalls)
 
 
 // User inputs congressperson's name and clicks SEARCH
+// Strings should concatenate
+const $form = $("form")
 
-$("form").on("submit", (event) => {
-  console.log(event) // not sure yet
-
-  // prevent page refresh
-  event.preventDefault()
+$form.on("submit", event => {
+  event.preventDefault() // prevent page refresh
 
   // create formData object for the name search
     // get the data needed from formData
@@ -63,4 +73,19 @@ $("form").on("submit", (event) => {
   const fullRepName = `${formData.get("firstname")} ${formData.get("lastname")}`
   console.log(fullRepName) // testing testing, good so far
 
+
+// THIS WILL BASICALLY BE, IF THE REP'S FULL NAME
+  // IS IN THE SPECIFIC PART OF THE ARRAY, THEN...
+  /*
+  for (v of recentRollCalls){
+    (let i=0; i=4) 
+  }
+  recentRollCalls.forEach(i => )
+  console.log(recentRollCalls);
+  recentRollCalls.forEach(() => {
+    if (fullRepName === (recentRollCalls.results.votes.vote.positions[i].name)){
+      console.log(fullRepName)
+    }
+  })*/
+});
 })
